67b960fa9155908f7b34bd493b1f14b542b7114e
ayushdev351 <mrayushchauhan351@gmail.com>
2023-04-30 19:01:18 +0530

    navbar design done

--- vite-project/components/Navbar/Navbar.jsx
+++ vite-project/components/Navbar/Navbar.jsx
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useState } from "react";
 import "./Navbar.scss";
 
 const Navbar = () => {
+
+    const [active, setActive] = useState(false);
+    const [open, setOpen] = useState(false);
+
+    const isActive = () => {
+        window.scrollY > 0 ? setActive(true) : setActive(false);
+    }
+
+    useEffect(() => {
+        window.addEventListener("scroll", isActive);
+
+        return() => {
+            window.removeEventListener("scroll", isActive);
+        }
+    }, []);
+
+    const menuToggler = () => {
+        open ? setOpen(false) : setOpen(true);
+    }
+    const currentUser = {
+        id : 1,
+        username : "Ayush",
+        isSeller : true,
+    }
+
     return(
-        <div className="navbar">
+        <div className={active ? "navbar active" : "navbar"}>
             <div className="container">
                 <div className="logo">
                     <span className="text">Buckett</span>
@@ -12,10 +38,40 @@ const Navbar = () => {
                 <div className="links">
                     <span>Explore</span>
                     <span>Sign In</span>
-                    <span>Become Expert</span>
-                    <button>Join</button>
+                    {!currentUser?.isSeller && <span>Become Expert</span>}
+                    {!currentUser && <button>Join</button>}
+                    {currentUser && (
+                        <div className="user" onClick={menuToggler}>
+                            <img src = "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" alt = "face"/>
+                            <span>{currentUser?.username}</span>
+                            <div className = {!open ? "close options" : "options"}>
+                                {currentUser.isSeller && (
+                                    <>
+                                        <span>My Buckets</span>
+                                        <span>Create Bucket</span>
+                                    </>
+                                )}
+                                <span>Orders</span>
+                                <span>Messages</span>
+                                <span>Logout</span>
+                            </div>
+                        </div>
+                    )
+                    }
                 </div>
             </div>
+            {active &&
+                <>
+                <hr/>
+                <div className="menu">
+                    <span>Explore</span>
+                    <span>Sign In</span>
+                    <span>Become Expert</span>
+                    <span>Explore</span>
+                    <span>Sign In</span>
+                </div>
+            </>
+            }
         </div>
     )
 }
